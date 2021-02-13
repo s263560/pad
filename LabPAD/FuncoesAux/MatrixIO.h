05ccdda45902831e2ef74dfe1906bd01e441223e
Andre Gradvohl <gradvohl@ft.unicamp.br>
2021-02-13 10:05:51 -0300

    Remocao do diretorio LabPAD

--- LabPAD/FuncoesAux/MatrixIO.h
+++
@@ -1,3 +0,0 @@
-void printMatrix(long int *mat, unsigned int lines, unsigned int columns, char *fileName);
-
-long int *readMatrix(unsigned int lines, unsigned int columns, char *fileName);
--- LabPAD/FuncoesAux/MatrixOperationsOMP.h
+++
@@ -1,1 +0,0 @@
-long int *multiply(long int *A, long int *B, unsigned int linesA, unsigned int columns_lines, unsigned int columnsB);
